5872a8ff8058d3524bda2008ecb3d9cfbc2b536f
Nasir Abed <nasirabed@gmail.com>
2015-05-28 13:45:25 +0200

    Added SeekBar and SaturationBar. The first for brightness and the last for white (shades) selection.

--- app/src/main/java/android/nasirabed/com/colorpicker/MainActivity.java
+++ app/src/main/java/android/nasirabed/com/colorpicker/MainActivity.java
@@ -11,11 +11,13 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 import com.larswerkman.holocolorpicker.ColorPicker;
 import com.larswerkman.holocolorpicker.OpacityBar;
+import com.larswerkman.holocolorpicker.SaturationBar;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -27,7 +29,9 @@ public class MainActivity extends Activity implements ColorPicker.OnColorChanged
 
     private ColorPicker picker;
     private OpacityBar opacityBar;
+    private SaturationBar saturationBar;
     private Button button;
+    private SeekBar seekBar;
     private String ipAddress;
 
     @Override
@@ -43,10 +47,28 @@ public class MainActivity extends Activity implements ColorPicker.OnColorChanged
 
         picker = (ColorPicker) findViewById(R.id.picker);
         opacityBar = (OpacityBar) findViewById(R.id.opacitybar);
-        button = (Button) findViewById(R.id.button1);
+        saturationBar = (SaturationBar) findViewById(R.id.saturationbar);
+        button = (Button) findViewById(R.id.button);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
 
         picker.addOpacityBar(opacityBar);
+        picker.addSaturationBar(saturationBar);
         picker.setOnColorChangedListener(this);
+
+        seekBar.setMax(255);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                sendBrightness(seekBar.getProgress());
+            }
+        });
 
         EditText editText = (EditText) findViewById(R.id.editText);
         editText.setText(ipAddress);
@@ -61,13 +83,30 @@ public class MainActivity extends Activity implements ColorPicker.OnColorChanged
         });
     }
 
+    public void sendBrightness(int brightness) {
+        Log.v("ColorPicker", "Brightness:" + brightness);
+
+        try {
+            Log.v("ColorPicker", "Connecting to: " + ipAddress);
+            Socket socket = new Socket(ipAddress, 5204);
+            Log.v("ColorPicker", "Is connected:" + socket.isBound());
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            dataOutputStream.write(7);
+            dataOutputStream.write(brightness);
+            socket.close();
+        } catch (IOException ex) {
+            Log.v("ColorPicker", "Cannot connect");
+            ex.printStackTrace();
+        }
+    }
 
     public void onButtonClick(View v) {
         picker = (ColorPicker) findViewById(R.id.picker);
         int red = Color.red(picker.getColor());
         int green = Color.green(picker.getColor());
         int blue = Color.blue(picker.getColor());
-        Log.v("ColorPicker", "Red:" + red + "Green:" + green + "Blue:" + blue);
+        Log.v("ColorPicker", "Red:" + red + " Green:" + green + " Blue:" + blue);
 
         try {
             Log.v("ColorPicker", "Connecting to: " + ipAddress);
@@ -80,6 +119,8 @@ public class MainActivity extends Activity implements ColorPicker.OnColorChanged
             dataOutputStream.write(red);
             dataOutputStream.write(blue);
             socket.close();
+
+            picker.setOldCenterColor(picker.getColor());
         } catch (IOException ex) {
             Log.v("ColorPicker", "Cannot connect");
             ex.printStackTrace();
@@ -90,12 +131,4 @@ public class MainActivity extends Activity implements ColorPicker.OnColorChanged
     public void onColorChanged(int color) {
         //gives the color when it's changed.
     }
-
-//    public boolean onEditorAction(EditText editText, int actionId, KeyEvent keyEvent) {
-//        if(actionId != EditorInfo.IME_ACTION_DONE)
-//            return false;
-//
-//        ipAddress = editText.getText().toString();
-//        return true;
-//    }
 }
